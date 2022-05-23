226a7a60f07e2bffa3b06d4b43a8c424e4ef4380
dakotamum <dakotamum@gmail.com>
2022-05-23 09:30:02 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,12 +12,15 @@ sudo apt-get install tmux
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/ycm-core/YouCompleteMe ~/.vim/bundle/
 
-#install YouCompleteMe
-python3 ~/.vim/bundle/YouCompleteMe/install.py --clangd-completer
-
 #copy vimrc and tmux.conf files to home directory
 cp .vimrc ~/
 cp .tmux.conf ~/
+
+#install YouCompleteMe
+cd ~/.vim/bundle/YouCompleteMe/
+git submodule update --init --recursive
+python3 ~/.vim/bundle/YouCompleteMe/install.py --clangd-completer
+cd
 
 #install plugins from vimrc
 vim +PluginInstall +qall
